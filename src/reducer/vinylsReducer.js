3bb78db2b2c0a16f7811763f226092b783bc0ee7
jlesly <leslyj2@gmail.com>
2022-01-25 16:44:26 -0500

    updated reducer to include add vinyls

--- src/reducer/vinylsReducer.js
+++ src/reducer/vinylsReducer.js
@@ -2,6 +2,8 @@ export const vinylsReducer = (state =[], action) => {
     switch(action.type) {
         case 'FETCH_VINYLS':
             return action.payload
+        case 'ADD_VINYLS':
+            return [...state, action.payload]
             default:
                 return state
     }
