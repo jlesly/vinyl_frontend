e69f30e0dc2850ba96cba4857328f749badee967
jlesly <leslyj2@gmail.com>
2022-01-25 11:21:56 -0500

    changed to class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <h1>Welcome!</h1>
-  );
+
+class App extends Component{
+  render () {
+    return (
+      <p>Welcome!</p>
+    )
+  }
 }
 
 export default App;
