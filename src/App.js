670fb9745e0c119b8e74153be9df502af586a7aa
jlesly <leslyj2@gmail.com>
2022-01-25 17:38:19 -0500

    updated route exact paths

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 // import logo from './logo.svg';
 import './App.css';
-// import { Switch, Route } from 'react-router-dom'
 import React from 'react';
 import {connect} from 'react-redux';
 import CategoriesContainer from './containers/CategoriesContainer';
 import { fetchCategories } from './actions/categoriesActions'
 import VinylFormContainer from './containers/VinylFormContainer';
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 
 class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchCategories();
+  }
+
   render() {
     return (
-      <Router>
-        <div>
-          <ul>
-            <ol>
-              <Link to='/'>Home</Link>
-              <Link to='/vinyls/new'>Add a Vinyl</Link>
-            </ol>
-          </ul>
-        </div>
-        <Switch>
-          <Route exact path='/'>< Home /></Route>
-          <Route exact path='/vinyls/new' component={ VinylFormContainer } />
+      <div className='app'>
+        <Switch> 
+          <Route exact path='/' component={ Home }/>
+          <Route exact path='/vinyls/new' component={ VinylFormContainer }/>
+          <Route path='/categories' component={ CategoriesContainer} />
         </Switch>
-      </Router>
+      </div>
     );
   }
 }
